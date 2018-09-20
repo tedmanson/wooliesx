be0bd8942f27d788cba287bf60fa466d00a79390
Scott Tedmanson <scott.tedmanson@ymail.com>
2018-09-20 15:33:08 +1000

    Fixing auth routes

--- src/server/server.go
+++ src/server/server.go
@@ -13,11 +13,13 @@ func Run() {
 
 	e.GET("/user", getUser)
 
-	e.Use(userTokenAuthMiddleware)
 	e.Use(wooliesSDKMiddleware)
 
-	e.GET("/sort", getProducts)
-	e.GET("/trolleyCalculator", getTrolley)
+	token := e.Group("")
+	token.Use(userTokenAuthMiddleware)
+
+	token.GET("/sort", getProducts)
+	token.GET("/trolleyCalculator", getTrolley)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
