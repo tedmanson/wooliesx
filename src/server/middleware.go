cf6384b20d45264f49d03bcdeb9cd41cb6a7ced2
Scott Tedmanson <scott.tedmanson@ymail.com>
2018-09-20 15:59:42 +1000

    Adding user check to middleware.

--- src/server/middleware.go
+++ src/server/middleware.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo"
 	"github.com/tedmanson/wooliesx/src/wooliesx"
@@ -9,6 +10,18 @@ import (
 
 func userTokenAuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		if strings.HasSuffix(c.Request().URL.Path, "user") {
+			var user = struct {
+				User  string `json:"user"`
+				Token string `json:"token"`
+			}{
+				User:  "Scott Tedmanson",
+				Token: "c722c138-b07f-4ee5-8c6f-7d603d552479",
+			}
+
+			return c.JSON(http.StatusOK, user)
+		}
+
 		if c.QueryParam("token") == "" {
 			return echo.NewHTTPError(http.StatusUnauthorized, "Please provide valid credentials")
 		}
